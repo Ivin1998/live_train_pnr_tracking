a4d751da78bd685dab505251473f86b192affcb2
Ivin_Austan <ivin.austan@capestart.com>
2023-05-11 22:01:10 +0530

    Live train pnr tracking

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,8 +1,9 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -21,24 +22,25 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		/* $this->load->helper('url'); */
 		$this->load->view('view');
 	}
 	public function get_status()
-	{	
-		$pnr =$_GET['pnr'];
-		$url = 'https://real-time-pnr-status-api-for-indian-railways.p.rapidapi.com/indianrail/'.$pnr;
+	{
+		$pnr = $_GET['pnr'];
+		$url = 'https://real-time-pnr-status-api-for-indian-railways.p.rapidapi.com/indianrail/' . $pnr;
 		$this->load->library('Guzzleclient');
 
 		$client = new \GuzzleHttp\Client();
-		
-		$response = $client->request('GET', $url , [
+
+		$response = $client->request('GET', $url, [
 			'headers' => [
 				'X-RapidAPI-Host' => 'real-time-pnr-status-api-for-indian-railways.p.rapidapi.com',
-				'X-RapidAPI-Key' => 'f0142f16e1mshda4b3ea0f466ffcp186bc5jsna333d3538d57',
+				'X-RapidAPI-Key' => 'b2a00d00a5msh10db23cbb5c7053p10aa46jsn6773b35273e2',
 			],
 		]);
-		
-	echo $response->getBody();
+
+		echo $response->getBody();
 
 	}
 }
--- application/views/view.php
+++ application/views/view.php
@@ -1,26 +1,50 @@
 <html>
-
 <head>
 	<title>PNR Status</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-
 </head>
 <body style="background-color:#ebebeb">
 	<div class="row" style="margin-top:10%">
 		<div class="container">
 			<div class="col col-md-12">
+			<img src="<?php ?>" alt="My Image">
+
 				<h3 class="text-center bg-primary" style="padding: 7px;">
 					Passenger Current Status Enquiry
-					</h3>
-					<input type="text" class="pnr" placeholder="ENTER PNR No." />
+				</h3>
+				<div style="text-align: center;"><input type="text" class="pnr" placeholder="ENTER PNR No." />
 					<button type="button" id="status" class="btn btn-success">Get your train status</button>
-
-					<p class="trainName"></p>
-
 				</div>
+				<table border="1" class="table table-bordered table-responsive text-center" id="journeyDetailsTable">
+					<thead>
+						<tr class="bg-primary">
+							<th><b> Train Number</b></th>
+							<th><b>Train Name</b></th>
+							<th><b>Journey_date Date <br>(DD-MM-YYYY)</b></th>
+							<th><b>Boarding Point</b></th>
+							<th><b>reservationUpto</b></th>
+							<th><b>Distance</b></th>
+							<th><b>Number of passengers</b></th>
+							<th><b>chartStatus</b></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td class="train_number"></td>
+							<td class="train_name"></td>
+							<td class="journey_date"></td>
+							<td class="boardingPoint"></td>
+							<td class="reservationUpto"></td>
+							<td class="distance"></td>
+							<td class="numberofpassengers"></td>
+							<td class="chartStatus"></td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
 		</div>
+	</div>
 </body>
 <script>
 	$(document).ready(function () {
@@ -31,12 +55,17 @@
 				dataType: "JSON",
 				data: { pnr: pnr },
 				success: function (data) {
-					$('.trainName').html(data.trainName);
-
+					$('.train_name').html(data.trainName);
+					$('.train_number').html(data.trainNumber);
+					$('.journey_date').html(data.dateOfJourney);
+					$('.distance').html(data.distance);
+					$('.numberofpassengers').html(data.numberOfpassenger);
+					$('.boardingPoint').html(data.boardingPoint);
+					$('.reservationUpto').html(data.reservationUpto);
+					$('.chartStatus').html(data.chartStatus);
 				}
 			});
 		});
 	});
 </script>
-
 </html>
